c13e68f093bdb86f7dfb3cced83d8f5ac8857cc7
David Fernandez <davidaf@utexas.edu>
2018-11-19 09:10:33 -0600

    first commit for BST, project is almost finished, needs to be fixed in linux

---
+++ BST312_driver.cpp
@@ -0,0 +1,8 @@
+//
+//  BST312_driver.cpp
+//  
+//
+//  Created by David Fernandez on 11/16/18.
+//
+
+#include <stdio.h>
---
+++ cmake-build-debug/CMakeFiles/BST.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BST.dir/BST312_driver.cpp.o"
+  "CMakeFiles/BST.dir/main.cpp.o"
+  "BST.pdb"
+  "BST"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BST.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
